63f622278ff2846df622f7e6efdc303b27d01e44
LAPTOP-08S49F46 <164348931@qq.com>
2019-03-14 16:22:04 +0800

    2019-03-01 1、添加调试界面框架 2、添加调试界面密码登陆功能 版本定义为0.7.0

---
+++ FullAutoQT1000/.svn/pristine/ed/ede8ec555d309e8ed9b84b372065c5407fd6e79d.svn-base
@@ -0,0 +1,31 @@
+﻿#ifndef DEBUGINTEFACEDLG_H
+#define DEBUGINTEFACEDLG_H
+
+#include <QDialog>
+#include <QDesktopWidget>
+
+namespace Ui {
+class debugIntefaceDlg;
+}
+
+class debugIntefaceDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit debugIntefaceDlg(QWidget *parent = 0);
+    ~debugIntefaceDlg();
+
+private slots:
+    void on_lw_debug_Navigate_currentRowChanged(int currentRow);
+
+private:
+    Ui::debugIntefaceDlg *ui;
+private:
+    //初始化导航栏
+    void InitNavigate();
+    //初始化对话框样式
+    void InitDialogStyle();
+};
+
+#endif // DEBUGINTEFACEDLG_H
---
+++ FullAutoQT1000/debugintefacedlg.cpp
@@ -0,0 +1,77 @@
+﻿#include "debugintefacedlg.h"
+#include "ui_debugintefacedlg.h"
+
+debugIntefaceDlg::debugIntefaceDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::debugIntefaceDlg)
+{
+    ui->setupUi(this);
+    //初始化对话框样式
+    InitDialogStyle();
+    //初始化导航栏
+    InitNavigate();
+
+}
+
+debugIntefaceDlg::~debugIntefaceDlg()
+{
+    delete ui;
+}
+
+/********************************************************
+ *@Name:        InitNavigate
+ *@Author:      HuaT
+ *@Description: 初始化调试界面导航栏
+ *@Param1:      无
+ *@Return:      无
+ *@Version:     1.0
+ *@Date:        2019-3-4
+********************************************************/
+void debugIntefaceDlg::InitNavigate()
+{
+    QStringList listNavigate;
+    listNavigate<<"常规调试"<<"参数设置"<<"数据库操作"<<"退出";
+    for(int n=0; n<listNavigate.size(); n++){
+        QListWidgetItem* item = new QListWidgetItem(listNavigate.at(n));
+        item->setSizeHint(QSize(187,80));
+        ui->lw_debug_Navigate->addItem(item);
+    }
+}
+
+/********************************************************
+ *@Name:        InitDialogStyle
+ *@Author:      HuaT
+ *@Description: 初始化对话框样式
+ *@Param1:      无
+ *@Return:      无
+ *@Version:     1.0
+ *@Date:        2019-3-4
+********************************************************/
+void debugIntefaceDlg::InitDialogStyle()
+{
+    this->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+    QDesktopWidget* dw = qApp->desktop();
+    this->move( (dw->width() - this->width())/2 , (dw->height() - this->height())/2);
+}
+
+/*
+ *
+ * 导航项目更改事件
+ *
+ */
+void debugIntefaceDlg::on_lw_debug_Navigate_currentRowChanged(int currentRow)
+{
+    switch(currentRow){
+    case 0:
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        this->close();
+        break;
+    default:
+        break;
+    }
+}
